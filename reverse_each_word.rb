ba3fa06d57dc0791a2a80934889d8650edeb24d7
Andrew Mullan <admullan@gmail.com>
2020-08-04 06:33:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
 def reverse_each_word(original_string)
-  reversed_array = []
   string_array = original_string.split(" ")
-  string_array.each_with_index do |str,index|
-    reversed_array[index] = str.reverse
+  string_array.collect do |str|
+    str.reverse
   end
+  binding.pry
   return reversed_array.join(" ")
 end
