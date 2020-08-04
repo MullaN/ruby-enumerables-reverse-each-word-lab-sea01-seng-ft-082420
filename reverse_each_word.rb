3fde1fa2f062a8d3967633528385894c1c6f1824
Andrew Mullan <admullan@gmail.com>
2020-08-04 06:26:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def reverse_each_word(original_string)
   string_array = original_string.split(" ")
-  string_array.each do |str|
+  string_array.each! do |str|
     str.reverse
   end
 end
