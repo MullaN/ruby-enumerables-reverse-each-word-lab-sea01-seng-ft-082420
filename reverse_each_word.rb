30acbeab06bc81cf631d3ec1c89c3f0bc204412b
Andrew Mullan <admullan@gmail.com>
2020-08-04 06:30:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def reverse_each_word(original_string)
-  reversed_arry = []
+  reversed_array = []
   string_array = original_string.split(" ")
   string_array.each_with_index do |str,index|
     reversed_array[index] = str.reverse
